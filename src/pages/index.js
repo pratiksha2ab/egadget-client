5f43f504f87278681c4db890856f8a0360312dfd
Manoj Pudasaini <mpudasaini17@gmail.com>
2021-05-26 18:53:22 +0545

    carousel added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>NepPharm</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <main className="max-w-screen-xl mx-auto">
+        <Banner />
+      </main>
     </div>
   )
 }

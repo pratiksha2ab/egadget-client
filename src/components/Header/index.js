780deb4d56afee543947ea7dbe81411d5c05361b
Manoj Pudasaini <mpudasaini17@gmail.com>
2021-05-27 18:42:32 +0545

    product feed created

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,11 +6,12 @@ function Header() {
         <header className="">
             {/* top nav */}
             <div className="flex items-center bg-gradient-to-tl from-green-500 to-green-700 p-1 flex-grow py-2 ">
-                <div className="my-2 px-2 flex items-center flex-grow sm:flex-grow-0">
+                <div className="my-1 flex items-center flex-grow sm:flex-grow-0">
                     <Image 
                         src='/neppharm.png'
                         width={150}
                         height={50}
+                        quality="100"
                         objectFit="contain"
                         className="cursor-pointer"
                         />
@@ -33,13 +34,13 @@ function Header() {
                 </div>
             </div>
             {/* bottom nav */}
-            <div className=" p-2 bg-gradient-to-b from-blue-400 to-blue-300 sm:hidden">
+            <div className=" p-2 bg-gradient-to-b from-green-500 to-green-600 sm:hidden">
                 <div className="flex items-center h-8 rounded-md flex-grow cursor-pointer bg-blue-400 hover:bg-blue-500 ">
                     <input className="p-2  h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text"/>
                     <SearchIcon className="h-12 p-4"/>
                 </div>
             </div>
-            <div className="flex text-center text-xs bg-gradient-to-b from-green-500 to-green-600 justify-around md:text-sm text-white tracking-wide font-semibold items-center  p-2">
+            <div className="flex text-center text-xs bg-gradient-to-b from-green-500 to-green-600 justify-around md:text-sm text-white tracking-wide font-semibold items-center p-2">
                 <div className="link">
                     <p>Medicines</p>
                 </div>
---
+++ src/components/ProductFeed/index.js
@@ -0,0 +1,24 @@
+import Product from "../Product"
+
+function ProductFeed({products}) {
+    return (
+        <div className=" grid grid-flow-row-dense md:grid-cols-4 lg:grid-col-4 xl:grid-col-5 md:-mt-45">
+          {
+            products.map(({id, title, price, description, category, image})=>(
+              <Product
+                key={id}
+                id={id}
+                title={title}
+                category={category}
+                price={price}
+                description={description}
+                image={image}
+                />
+            )
+            )
+          }
+        </div>
+    )
+}
+
+export default ProductFeed
